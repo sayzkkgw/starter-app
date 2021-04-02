94901632f5076f77b715c3789b6038f7e3d56816
yururin <sayzkkgw@gmail.com>
2021-04-02 19:15:36 +0900

    Create _buildSuggestions Widget

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,24 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final wordPair = WordPair.random();
